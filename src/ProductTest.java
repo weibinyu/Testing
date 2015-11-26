382d173d07028062fa12a378af8added809920f6
weibinyu <a1994218@hotmail.com>
2015-11-26 23:48:36 +0800

    RED

--- src/ProductTest.java
+++ src/ProductTest.java
@@ -9,4 +9,11 @@ public class ProductTest {
 	public void shouldCreateProduct() {
 	Product p = new Product("TV", 5000);
 	}
+	
+	@Test
+	public void shouldReturnCorrectProduct() {
+		Product p = new Product("Milk", 30);
+		assertEquals("Milk", p.getProduct());
+		assertEquals(30, p.getPrice());
+	}
 }
