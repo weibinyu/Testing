31aecbbaa8cac9b904881f8fea6aa79a8a8780bd
weibinyu <a1994218@hotmail.com>
2015-11-27 00:16:14 +0800

    RED

---
+++ src/ShoppingCartTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ShoppingCartTest {
+	@Test
+	public void CreateInstanceOfRegister() {
+		ShoppingCart cart = new ShoppingCart();
+		assertEquals(0, cart.getSum());
+	}
+}
