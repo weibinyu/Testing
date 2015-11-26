9a55ed6864207ca04ce378586bdb6d0aa8a8036a
weibinyu <a1994218@hotmail.com>
2015-11-26 23:20:37 +0800

    RED

---
+++ src/ProductTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class ProductTest {
+	@Test
+	public void shouldCreateProduct() {
+	Product p = new Product("TV", 5000);
+	}
+}
