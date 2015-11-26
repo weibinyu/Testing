21f864c09e2645f5d3a95b918ca87841432e61fc
weibinyu <a1994218@hotmail.com>
2015-11-27 03:03:06 +0800

    RED

--- src/ShoppingCartTest.java
+++ src/ShoppingCartTest.java
@@ -3,6 +3,10 @@ import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ShoppingCartTest {
 	@Test
 	public void CreateInstanceOfRegister() {
@@ -18,6 +22,16 @@ public class ShoppingCartTest {
 		cart.addItem(milk);
 		Mockito.verify(milk).getPrice();
 		assertEquals(30, cart.getTotalPrice());
+	}
+	
+	@Test
+	public void shouldAddItemToRegister() {
+		ShoppingCart cart = new ShoppingCart();
+		List<Product> Products = new ArrayList<Product>();
+		Product milk = Mockito.mock(Product.class)
+		Products.add(milk);
+		cart.addItem(milk);
+		assertEquals(Products, cart.getProducts());
 	}
 }
 
